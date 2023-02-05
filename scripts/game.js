659d4a358244c11f59d61a4ee56086f3f96802ed
Myles <mchanin.uvic@gmail.com>
2023-02-04 17:39:19 -0800

    Complete functionality in game.js, Remove utilities.css as it was not used

--- scripts/game.js
+++ scripts/game.js
@@ -1,10 +1,79 @@
 const gridSize = document.getElementById("gridSlider");
 const sliderText = document.getElementById("grid-size-counter");
+const penInput = document.getElementById("color-picker-pen");
+const bgInput = document.getElementById("color-picker-bg");
+const boardButtons = document.getElementsByClassName("board-coloring")
+
+let currentColor = "red";
+let penColor = "black";
+let bgColor = "white";
+
+let random = false;
+let eraser = false;
+let eyeDropper = false;
+let ice = false;
+let fire = false;
 
 gridSize.addEventListener('input', updateGrid);
 
 genBoard(gridSize.value);
 updateGridText();
+
+//Document event listeners
+
+document.addEventListener('click', function(event) {
+    const target = event.target.closest("button");
+    if(!target.classList.contains("clear")) {
+        if(target.classList.contains("active")) {
+            target.classList.remove("active");
+        } else {
+            if(target.classList.contains("board-coloring")) {
+                for(let i = 0; i < boardButtons.length; i++) {
+                    boardButtons[i].classList.remove("active");
+                }
+            }
+            target.classList.add("active");    
+        }
+    }
+})
+
+document.addEventListener('mouseover', function(event) {
+    if(!eyeDropper) {
+        const target = event.target.closest(".gridSquare");
+        if(random) {
+            rotateColor();
+        } else if(eraser) {
+            currentColor = "white";
+        } else if(ice) {
+            iceColor();
+        } else if(fire) {
+            fireColor();
+        }else {
+            currentColor = penColor;
+        }
+        target.style.backgroundColor = currentColor;
+    }
+});
+
+document.addEventListener('click', function(event) {
+    if(eyeDropper) {
+        const target = event.target.closest(".gridSquare");
+        penColor = target.style.backgroundColor;
+        penInput.parentNode.style.backgroundColor = penColor;
+    }
+});
+
+penInput.addEventListener('change', function() {
+    penColor = penInput.value;
+    penInput.parentNode.style.backgroundColor = penInput.value;
+});
+
+bgInput.addEventListener('change', function() {
+    bgColor = bgInput.value;
+    bgInput.parentNode.style.backgroundColor = bgInput.value;
+});
+
+//Grid functions
 
 function updateGrid() {
     let newSize = gridSize.value;
@@ -46,7 +115,7 @@ function genBoard(numRows) {
 function clearBoard() {
     let gridSquares = document.getElementsByClassName("gridSquare");
     for(let i = 0; i < gridSquares.length; i++) {
-        gridSquares[i].style.backgroundColor = "white";
+        gridSquares[i].style.backgroundColor = bgColor;
     }
 };
 
@@ -62,4 +131,60 @@ function clearGridLines() {
     }    
 }
 
+//Color functions
+
+function rotateColor() {
+    let r = Math.random()*255;
+    let g = Math.random()*255;
+    let b = Math.random()*255;
+    currentColor = "rgb(" + r +"," + g +"," + b + ")";
+}
+
+function iceColor() {
+    let r = 0;
+    let g = Math.random()*255;
+    let b = 255;
+    currentColor = "rgb(" + r +"," + g +"," + b + ")";    
+}
+
+function fireColor() {
+    let r = 255;
+    let g = Math.random()*255;
+    let b = 0;
+    currentColor = "rgb(" + r +"," + g +"," + b + ")"; 
+}
+
+//Toggle functions
+
+function randomToggle() {
+    eraser = false;
+    ice = false;
+    fire = false;
+    random = !random;
+}
+
+function eraserToggle() {
+    random = false;
+    ice = false;
+    fire = false;    
+    eraser = !eraser;
+}
+
+function eyeDrop() {
+    eyeDropper = !eyeDropper;
+}
+
+function iceToggle() {
+    random = false;
+    eraser = false;
+    fire = false;     
+    ice = !ice;
+}
+
+function fireToggle() {
+    random = false;
+    ice = false;
+    eraser = false;     
+    fire = !fire;
+}
 
